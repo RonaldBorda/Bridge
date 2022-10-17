03546a77d44504bc434431c7ad99b7d6ab6e0447
RonaldBorda <ronald.borda@tecsup.edu.pe>
2022-10-17 10:54:08 -0500

    Add project files.

---
+++ Bridge/SamsungLedTv.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge
+{
+    public class SamsungLedTv : LEDTV
+    {
+        public void SetChannel(int channetNumber)
+        {
+            Console.WriteLine("Configuración de canal " + channetNumber + " en Samsung Tv");
+        }
+
+        public void SwitchOff()
+        {
+            Console.WriteLine("Apagado: Samsung TV");
+        }
+
+        public void SwitchOn()
+        {
+            Console.WriteLine("Encendido: Samsung TV");
+        }
+    }
+}
---
+++ Bridge/SonyLedTv.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge
+{
+    public class SonyLedTv : LEDTV
+    {
+        public void SetChannel(int channetNumber)
+        {
+            Console.WriteLine("Configuración de canal " + channetNumber + " en Sony Tv");
+        }
+
+        public void SwitchOff()
+        {
+            Console.WriteLine("Apagado: Sony TV");
+        }
+
+        public void SwitchOn()
+        {
+            Console.WriteLine("Encendido: Sony TV");
+        }
+    }
+}
